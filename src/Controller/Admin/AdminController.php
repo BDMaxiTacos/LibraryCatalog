dc986e75316503251e4bed974e2128fab0269ec9
BDMaxiTacos <HideSnake.Gaming@gmail.com>
2023-10-20 21:15:11 +0200

    admin for books (add, edit and delete)

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Book;
+use App\Form\BookType;
 use App\Repository\AuthorRepository;
 use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,21 +24,98 @@ class AdminController extends AbstractController
         ]);
     }
 
-    #[Route('/admin/books', name: 'admin_books')]
-    public function books(BookRepository $bookRepository): Response
-    {
-        $user = $this->getuser();
+    /** Books routes */
+        #[Route('/admin/books', name: 'admin_books')]
+        public function books(
+            BookRepository $bookRepository
+        ): Response
+        {
+            $user = $this->getuser();
 
-        $books = $bookRepository->findAll();
-        
-        return $this->render('admin/book/index.html.twig', [
-            'user' => $user,
-            'books' => $books
-        ]);
-    }
+            $books = $bookRepository->findAll();
+            
+            return $this->render('admin/book/index.html.twig', [
+                'user' => $user,
+                'books' => $books
+            ]);
+        }
+
+        #[Route('/admin/books/edit/{id}', name: 'admin_book_edit')]
+        public function editBook(
+            $id, 
+            BookRepository $bookRepository, 
+            Request $request, 
+            EntityManagerInterface $em
+        ): Response
+        {
+            $book = $bookRepository->findOneBy(['id' => $id]);
+            $form = $this->createForm(BookType::class, $book);
+            $form->handleRequest($request);
+
+            if($form->isSubmitted() && $form->isValid()){
+                $book = $form->getData();
+
+                $em->flush();
+                $this->addFlash('success', 'Le livre a bien été modifié');
+                return $this->redirectToRoute('admin_books');
+            }
+            
+            return $this->render('admin/book/form.html.twig', [
+                'form' => $form,
+                'title' => 'Modifier un livre'
+            ]);
+        }
+
+        #[Route('/admin/books/delete/{id}', name: 'admin_book_delete')]
+        public function deleteBook(
+            Book $book, 
+            Request $request, 
+            EntityManagerInterface $em
+        ): Response
+        {
+            if($this->isCsrfTokenValid('delete' . $book->getId(), $request->get('_token'))){
+                $em->remove($book);
+                $em->flush();
+                $this->addFlash('success', 'Votre article a bien été supprimé');
+            }else{
+                $this->addFlash('success', 'Impossible de supprimer l\'article');
+            }
+            return $this->redirectToRoute('admin_books');
+        }
+
+        #[Route('/admin/books/add', name: 'admin_book_add')]
+        public function addBook(
+            Request $request, 
+            EntityManagerInterface $em
+        ): Response
+        {
+            $book = new Book();
+            $form = $this->createForm(BookType::class, $book);
+            $form->handleRequest($request);
+
+            if($form->isSubmitted() && $form->isValid()){
+                $book = $form->getData();
+
+                $book->setPublicationDate(new \DateTime());
+                
+                $em->persist($book);
+                $em->flush();
+
+                $this->addFlash('success', 'Le livre a bien été ajouté');
+                return $this->redirectToRoute('admin_books');
+            }
+            
+            return $this->render('admin/book/form.html.twig', [
+                'form' => $form,
+                'title' => 'Ajouter un livre'
+            ]);
+        }
+    /** ---- */
 
     #[Route('/admin/authors', name: 'admin_authors')]
-    public function authors(AuthorRepository $authorRepository): Response
+    public function authors(
+        AuthorRepository $authorRepository
+    ): Response
     {
         $user = $this->getuser();
 
---
+++ src/Form/BookType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Author;
+use App\Entity\Book;
+use App\Entity\Category;
+use App\Repository\AuthorRepository;
+use App\Repository\CategoryRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'required' => true,
+            ])
+            ->add('catchphrase', TextareaType::class, [
+                'required' => true,
+            ])
+            ->add('summary', TextareaType::class, [
+                'required' => true
+                ])
+            ->add('authors', EntityType::class, [
+                'class' => Author::class,
+                'query_builder' => function(AuthorRepository $authorRepository){
+                    return $authorRepository->createQueryBuilder('a')
+                              ->orderBy('a.name', 'ASC');
+                },
+                'choice_label' => 'name',
+                'multiple' => true,
+                'required' => true
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'query_builder' => function(CategoryRepository $categoryRepository){
+                    return $categoryRepository->createQueryBuilder('c')
+                              ->orderBy('c.name', 'ASC');
+                },
+                'choice_label' => 'name',
+                'required' => true
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Book::class,
+        ]);
+    }
+}
