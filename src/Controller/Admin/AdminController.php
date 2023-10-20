2215e2c3f53c71094e8baf370b4174abba932a02
BDMaxiTacos <HideSnake.Gaming@gmail.com>
2023-10-20 23:10:46 +0200

    admin for authors side, css to match authors side

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Author;
 use App\Entity\Book;
+use App\Form\AuthorType;
 use App\Form\BookType;
 use App\Repository\AuthorRepository;
 use App\Repository\BookRepository;
@@ -42,13 +44,11 @@ class AdminController extends AbstractController
 
         #[Route('/admin/books/edit/{id}', name: 'admin_book_edit')]
         public function editBook(
-            $id, 
-            BookRepository $bookRepository, 
+            Book $book,
             Request $request, 
             EntityManagerInterface $em
         ): Response
         {
-            $book = $bookRepository->findOneBy(['id' => $id]);
             $form = $this->createForm(BookType::class, $book);
             $form->handleRequest($request);
 
@@ -78,7 +78,7 @@ class AdminController extends AbstractController
                 $em->flush();
                 $this->addFlash('success', 'Votre article a bien été supprimé');
             }else{
-                $this->addFlash('success', 'Impossible de supprimer l\'article');
+                $this->addFlash('error', 'Impossible de supprimer l\'article');
             }
             return $this->redirectToRoute('admin_books');
         }
@@ -112,18 +112,89 @@ class AdminController extends AbstractController
         }
     /** ---- */
 
-    #[Route('/admin/authors', name: 'admin_authors')]
-    public function authors(
-        AuthorRepository $authorRepository
-    ): Response
-    {
-        $user = $this->getuser();
+    /** Author routes */
+        #[Route('/admin/authors', name: 'admin_authors')]
+        public function authors(
+            AuthorRepository $authorRepository
+        ): Response
+        {
+            $user = $this->getuser();
 
-        $authors = $authorRepository->findAll();
+            $authors = $authorRepository->findAll();
+            
+            return $this->render('admin/author/index.html.twig', [
+                'user' => $user,
+                'authors' => $authors
+            ]);
+        }
         
-        return $this->render('admin/author/index.html.twig', [
-            'user' => $user,
-            'authors' => $authors
-        ]);
-    }
+        #[Route('/admin/authors/edit/{id}', name: 'admin_author_edit')]
+        public function editAuthor(
+            Author $author,
+            Request $request, 
+            EntityManagerInterface $em
+        ): Response
+        {
+            $form = $this->createForm(AuthorType::class, $author);
+            $form->handleRequest($request);
+
+            if($form->isSubmitted() && $form->isValid()){
+                $author = $form->getData();
+
+                $em->flush();
+                $this->addFlash('success', 'L\'auteur a bien été modifié');
+                return $this->redirectToRoute('admin_authors');
+            }
+            
+            return $this->render('admin/author/form.html.twig', [
+                'form' => $form,
+                'title' => 'Modifier un auteur'
+            ]);
+        }
+
+        #[Route('/admin/authors/delete/{id}', name: 'admin_author_delete')]
+        public function deleteAuthor(
+            Author $author, 
+            Request $request, 
+            EntityManagerInterface $em
+        ): Response
+        {
+            if($this->isCsrfTokenValid('delete' . $author->getId(), $request->get('_token'))){
+                $em->remove($author);
+                $em->flush();
+                $this->addFlash('success', 'L\'auteur a bien été supprimé');
+            }else{
+                $this->addFlash('error', 'Impossible de supprimer l\'auteur');
+            }
+            return $this->redirectToRoute('admin_authors');
+        }
+
+        #[Route('/admin/authors/add', name: 'admin_author_add')]
+        public function addAuthor(
+            Request $request, 
+            EntityManagerInterface $em
+        ): Response
+        {
+            $author = new Author();
+            $form = $this->createForm(AuthorType::class, $author);
+            $form->handleRequest($request);
+
+            if($form->isSubmitted() && $form->isValid()){
+                $author = $form->getData();
+
+                $author->setPublicationDate(new \DateTime());
+                
+                $em->persist($author);
+                $em->flush();
+
+                $this->addFlash('success', 'L\auteur a bien été ajouté');
+                return $this->redirectToRoute('admin_authors');
+            }
+            
+            return $this->render('admin/author/form.html.twig', [
+                'form' => $form,
+                'title' => 'Ajouter un auteur'
+            ]);
+        }
+    /** ---- */
 }
