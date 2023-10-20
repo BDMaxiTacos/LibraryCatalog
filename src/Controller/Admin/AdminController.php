0b670c53f558bf6e6fe29569ad38be9091e590de
BDMaxiTacos <HideSnake.Gaming@gmail.com>
2023-10-20 16:07:45 +0200

    add base routes for books and authors

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\AuthorRepository;
+use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +15,34 @@ class AdminController extends AbstractController
     {
         $user = $this->getuser();
         
-        return $this->render('admin/home.html.twig', [
+        return $this->render('admin/index.html.twig', [
             'user' => $user
+        ]);
+    }
+
+    #[Route('/admin/books', name: 'admin_books')]
+    public function books(BookRepository $bookRepository): Response
+    {
+        $user = $this->getuser();
+
+        $books = $bookRepository->findAll();
+        
+        return $this->render('admin/book/index.html.twig', [
+            'user' => $user,
+            'books' => $books
+        ]);
+    }
+
+    #[Route('/admin/authors', name: 'admin_athors')]
+    public function authors(AuthorRepository $authorRepository): Response
+    {
+        $user = $this->getuser();
+
+        $authors = $authorRepository->findAll();
+        
+        return $this->render('admin/author/index.html.twig', [
+            'user' => $user,
+            'authors' => $authors
         ]);
     }
 }
